4d6faed9e60e4e2038c6042ddea1cc38d9299452
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-10-12 21:07:31 +0700

    Initial commit

---
+++ lib/Bash/History/Read.pm
@@ -0,0 +1,92 @@
+package Bash::History::Read;
+
+# DATE
+# VERSION
+
+use 5.010;
+use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT = qw(each_hist);
+
+sub each_hist(&) {
+    my $code = shift;
+
+    my $call_code = sub {
+        my ($ts, $content) = @_;
+        package main {
+            local $_ = $content;
+            local $main::TS = $ts;
+            local $main::PRINT = 1;
+            $code->();
+            if ($main::PRINT) {
+                print "#$ts\n$_";
+            }
+        }
+    };
+
+    my $ts;
+    my $content = "";
+    while (my $line = <>) {
+        if ($line =~ /\A#(\d+)$/) {
+            $ts = $1;
+            $call_code->($ts, $content) if length($content);
+            $content = '';
+            next;
+        } elsif (defined $ts) {
+            $content .= $line;
+        } else {
+            die "Invalid input, timestamp line expected";
+        }
+    }
+    if (defined($ts) && length($content)) {
+        $call_code->($ts, $content);
+    }
+}
+
+1;
+# ABSTRACT: Utility to read bash history file entries
+
+=head1 SYNOPSIS
+
+From the command-line:
+
+ % perl -MBash::History::Read -i.bak -e'each_hist {
+       $PRINT = 0 if $TS < time()-2*30*86400; # delete old entries
+       $PRINT = 0 if /foo/; # delete unwanted lines (e.g. matching some regex)
+       s/(mysql\s+-p)(\S+)/$1******/; # redact sensitive information
+   }' ~/.bash_history
+
+
+=head1 DESCRIPTION
+
+This module provides utility routines to read entries from bash history file (by
+default C<~/.bash_history>). The format of the history file is dead simple: one
+line per entry, but when C<HISTTIMEFORMAT> environment is set, bash will print a
+timestamp line before each entry, e.g.:
+
+ #1374290613
+ ls -al
+ #1374290618
+ less myfile
+ #1374290635
+ ...
+
+See C<each_hist> for one routine to let you handle this format conveniently.
+
+
+=head1 FUNCTIONS
+
+=head2 each_hist { PERL_CODE }
+
+Will read lines from the diamond operator (C<< <> >>) and call Perl code for
+each history entry. Can handle timestamp lines. This routine is exported by
+default and is meant to be used from one-liners.
+
+Inside the Perl code, C<$_> is locally set to the entry content, C<$TS> is
+locally set to the timestamp (and cannot be changed), C<$PRINT> is locally set
+to 1. If C<$PRINT> is still true by the time the Perl code ends, the entry
+(along with its timestamp) will be printed to STDOUT. So to remove a line, you
+can set C<$PRINT> to 0 in your code. To modify content, modify the C<$_>
+variable.
